342752dc93455fe4e7b7d15e8d0be85d9e77bf0e
anasouss <anas.oussous11@gmail.com>
2024-07-14 17:13:45 +0100

    ChatBot

---
+++ src/main/java/net/oussous/ensetchatbotrag/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package net.oussous.ensetchatbotrag.services;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/test/java/net/oussous/ensetchatbotrag/EnsetChatbotRagApplicationTests.java
@@ -0,0 +1,13 @@
+package net.oussous.ensetchatbotrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsetChatbotRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
